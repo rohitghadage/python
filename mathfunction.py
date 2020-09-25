72c35fada2f480055bdd93d61a89178929738280
rohit vinayak ghadag <rohitghadage331@gmail.com>
2020-09-25 11:50:32 +0530

    mathfunction.py

---
+++ mathfunction.py
@@ -0,0 +1,17 @@
+# there are 3 methods to import math functon
+ # 1)direct
+import math
+x=math.sqrt(25)
+print(x)
+
+# 2) align
+import math as m
+y=m.pow(3,4)
+print(y)
+
+# 3) shortcut
+from math import sqrt,pow
+x=sqrt(25)
+y=pow(2,3)
+print(x)
+print(y)
