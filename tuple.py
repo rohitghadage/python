9b4ee9ff07659e9471fed7937e309f09974560b5
rohit vinayak ghadag <rohitghadage331@gmail.com>
2020-09-25 11:11:47 +0530

    tuple

---
+++ tuple.py
@@ -0,0 +1,7 @@
+""""  tuple-tuple is same as list.the difference is we use'(',')' that braces .
+       In tuple we cant perporm any operation."""
+
+tup=("rohit","vinayak","ghadage")
+print(tup)
+x=tup[1]
+print(x)
