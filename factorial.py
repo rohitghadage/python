9777224abc95bfff78b189fae1400ce443e95060
rohit vinayak ghadag <rohitghadage331@gmail.com>
2020-09-25 22:16:35 +0530

    factorial.py

--- factorial.py
+++ factorial.py
@@ -1,5 +1,16 @@
-n=int(input("enter the number which to be calculate fatorial of a number"))
+"""n=int(input("enter the number which to be calculate fatorial of a number"))
 fact=1
 for i in range(1,n+1):
     fact=fact * i
-print("factorial of num=",fact)
+print("factorial of num=",fact)  """
+
+ # using recursion
+n=int(input("enter the number which to be calculate fatorial of a number"))
+def fact(n):
+    if n==1 or n==0:
+        return n
+    else:
+        return n*fact(n-1)
+print("factorial=",fact(n))
+
+
