50c7c02e689f9e2d5a2a83e0684f5c69a75fac12
rohit vinayak ghadag <rohitghadage331@gmail.com>
2020-09-25 11:18:00 +0530

    sets

---
+++ sets.py
@@ -0,0 +1,8 @@
+"""" set- 1) use {,} braces
+           2)immutatable
+            3)unordered(order is not fix in output which is given to input)
+            4)duplicate elements not allows
+            5)in set we cant display any index elemennt"""
+
+s={10,20,30,40}
+print(s)
