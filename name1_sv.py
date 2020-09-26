8c9d9f948fa0f8cda78de518b5d7203465fd84f2
rohit vinayak ghadag <rohitghadage331@gmail.com>
2020-09-26 18:27:02 +0530

    name_sv.py

---
+++ name1_sv.py
@@ -0,0 +1,9 @@
+import name_sv as n
+#import standalone as sa
+
+#print("Running the imported script")
+n.sv()
+
+#print('\n')
+#print("Running the current script")
+print("The value of __name__ is ",  __name__)
---
+++ name_sv.py
@@ -0,0 +1,2 @@
+def sv():
+   print('The value of __name__ is ', __name__)
